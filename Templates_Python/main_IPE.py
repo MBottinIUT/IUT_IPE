9b55d8374309256a9ab3430235180690118a1d10
MBottinIUT <39375197+MBottinIUT@users.noreply.github.com>
2018-07-19 16:06:17 +0200

    Add files via upload

---
+++ Templates_Python/main_IPE.py
@@ -0,0 +1,20 @@
+#!usr/bin/python
+# -*- coding : utf-8 -*-
+
+"""
+nom du fichier : {filename}
+auteurs : {developer}
+Etablissement : {company}
+Date : {datetime}
+
+Commentaires : 
+"""
+
+# Programme principal
+while True :
+	try :
+		pass
+	except KeyboardInterrupt :
+		print "Au revoir..."
+		break
+ 
