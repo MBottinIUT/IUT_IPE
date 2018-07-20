5a4c7871c050f5ac542f9ebda7a5cd9a8241ec3b
MBottinIUT <39375197+MBottinIUT@users.noreply.github.com>
2018-07-20 15:46:56 +0200

    Add files via upload

---
+++ Templates_Python/main_TP1.py
@@ -0,0 +1,15 @@
+#!usr/bin/python
+# -*- coding : utf-8 -*-
+
+"""
+Module IPE - TPs Raspberry
+auteurs : {developer}
+Etablissement : {company}
+Date : {datetime}
+
+Commentaires : 
+"""
+
+# Programme principal
+
+ 
